04361431a7281c677c95d8022260c93fe56aca04
ryadbenmanser <ryadbenmanser@gmail.com>
2023-07-09 10:43:37 +0100

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    color: {
+      primary: "#CBA590",
+      secondary: "#54A292",
+      warm: "#1F5B4F",
+      cool: "#7F7C76",
+      complementary:''
+    },
+  },
+  plugins: [],
+};
